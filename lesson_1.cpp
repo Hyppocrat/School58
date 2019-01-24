abbb0a53b825adcb0895afc5c46af879990b1381
Hyppocrat <djsh92@mail.ru>
2019-01-24 19:33:48 +0300

    Create lesson_1.cpp

---
+++ lesson_1.cpp
@@ -0,0 +1,56 @@
+// шаблон для программы на c++
+#include <iostream> // подключение ввода-вывода
+#include <string>   // подключение библиотека для работы со строками
+
+using namespace std;
+
+int main(){
+	string s, s1, s2; // объявление строки	
+	cin >> s >> s1 >> s2; // считывание нескольких переменных
+	cout << s << ' ' << s1 << ' ' << s2; // вывод переменных через пробел
+	cout << s << endl << s1 << endl << s2 << endl; // вывод каждой переменной в отдельной строке
+	// подсчёт количества больших букв в строке
+	int cnt_up = 0;
+	for(int i = 0; i < s.size(); i++){
+		if (isupper(s[i])){
+			cnt_up++;
+		}
+	}
+	// подсчёт количества маленьких букв в строке
+	int cnt_low = 0;
+	for(int i = 0; i < s.size(); i++){
+		if (islower(s[i])){
+			cnt_low++;
+		}
+	}
+	// подсчёт количества цифр в строке
+	int cnt_dig = 0;
+	for(int i = 0; i < s.size(); i++){
+		if (isdigit(s[i])){
+			cnt_dig++;
+		}
+	}
+	// подсчёт количества букв (маленьких и больших) в строке
+	int cnt_al = 0;
+	for(int i = 0; i < s.size(); i++){
+		if (isalpha(s[i])){
+			cnt_al++;
+		}
+	}
+	// подсчёт количества букв и цифр в строке
+	int cnt_alnum = 0;
+	for(int i = 0; i < s.size(); i++){
+		if (isalnum(s[i])){
+			cnt_alnum++;
+		}
+	}
+	// обращение всех букв в строке в маленькие
+	for(int i = 0; i < s.size(); i++){
+		s[i] = tolower(s[i]);
+	}
+	// обращение всех букв в строке в большие
+	for(int i = 0; i < s.size(); i++){
+		s[i] = toupper(s[i]);
+	}
+}
+
