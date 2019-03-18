5070b3826cb9906b5670f5a5287407b7acba3ccc
Hyppocrat <djsh92@mail.ru>
2019-03-18 16:18:42 +0300

    Create lesson_9.cpp

---
+++ lesson_9.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+//#include <math.h>
+
+using namespace std;
+
+int main(){
+    //float x, y;
+    //double x, y;
+    long double x, y, z;
+    /*//x0, y0, x1, y1 - нельзя
+    cin >> x >> y;
+    cout.setf(ios::fixed);
+    cout.precision(4); // вывод с 4 знаками после запятой
+    cout << x << endl;
+    cout.precision(6);
+    cout << x;
+    */
+    /*int t;
+    cin >> t;
+    // так делать ваще не стоит
+    x = t / 2;
+    // 1 сп (так делать стоит)
+    x = t / 2.0;
+    // 2 сп (так делать стоит)
+    x = (double)t / 2;
+    // 3 сп (так делать стоит)
+    x = (t + 0.0) / 2;
+    // 4 сп (так делать стоит)
+    x = t * 1.0 / 2;*/
+    cin >> x;
+    // модуль числа
+    //x = fabs(x);
+    // квадратный корень
+    //x = sqrt(x);
+    // возведение в степень
+    //z = 1 / 2.0;
+    //z = 1 / 3.0;
+    //y = pow(x, z);
+    //округление вверх
+    //x = ceil(x);
+    //округление вниз
+    //x = floor(x);
+    //округление 
+    //x = floor(x + 0.5);
+    cout << cos(60.0);
+    cout << x;
+    
+}
