45cdb29019b46719f3ff1c7f76a517163eccbc96
Hyppocrat <djsh92@mail.ru>
2019-04-22 15:48:21 +0300

    Create lesson_14.cpp

---
+++ lesson_14.cpp
@@ -0,0 +1,13 @@
+#include <cstring>
+
+using namespace std;
+
+int main(){
+    //freopen("input.txt", "rt", stdin);
+    //freopen("output.txt", "wt", stdout);
+    
+}
+
+https://codeforces.com/problemset/problem/53/C?locale=ru
+https://codeforces.com/problemset/problem/78/B?locale=ru
+https://codeforces.com/problemset/problem/361/A?locale=ru
