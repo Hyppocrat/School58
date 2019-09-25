e84d91d2c995e424aa714b899dac5030ecffad67
Hyppocrat <djsh92@mail.ru>
2019-09-25 17:07:34 +0300

    Update test.py

--- test.py
+++ test.py
@@ -77,3 +77,45 @@ if __name__ == '__main__':
     wnd = Window()
     wnd.show()
     sys.exit(app.exec())
+
+    
+    ==========================================================
+    
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel
+from PyQt5.QtWidgets import QLCDNumber
+ 
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 300)
+        self.setWindowTitle('Пятая программа')
+
+        self.btn = QPushButton('Кнопка', self)
+        self.btn.resize(self.btn.sizeHint())
+        self.btn.move(100, 150)
+        self.btn.clicked.connect(self.hello)
+
+        self.label = QLabel(self)
+        self.label.setText("Количество нажатий на кнопку")
+        self.label.move(80, 30)
+
+        self.LCD_count = QLCDNumber(self)
+        self.LCD_count.move(110, 80)
+
+        self.count = 0
+
+    def hello(self):
+        self.count += 1
+        self.LCD_count.display(self.count)
+        
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
