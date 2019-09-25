04792c75f8f3b1b5bfbba2234d536c008beb122b
Hyppocrat <djsh92@mail.ru>
2019-09-25 17:18:30 +0300

    Update test.py

--- test.py
+++ test.py
@@ -122,3 +122,48 @@ if __name__ == '__main__':
     ex = Example()
     ex.show()
     sys.exit(app.exec())
+
+    
+    ==============================
+    
+    
+    import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel
+from PyQt5.QtWidgets import QLCDNumber, QLineEdit
+ 
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 300)
+        self.setWindowTitle('Шестая программа')
+
+        self.btn = QPushButton('Кнопка', self)
+        self.btn.resize(self.btn.sizeHint())
+        self.btn.move(100, 150)
+        self.btn.clicked.connect(self.hello)
+
+        self.label = QLabel(self)
+        self.label.setText("Привет, неопознанный лев")
+        self.label.move(40, 30)
+
+        self.name_label = QLabel(self)
+        self.name_label.setText("Введите имя: ")
+        self.name_label.move(40, 90)
+
+        self.name_input = QLineEdit(self)
+        self.name_input.move(130, 90)
+
+    def hello(self):
+        name = self.name_input.text()
+        self.label.setText("Привет, {}".format(name))
+        
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
