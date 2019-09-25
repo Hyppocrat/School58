180ecd7b5120f70ff1b0a849e50789bc63fc4b66
Hyppocrat <djsh92@mail.ru>
2019-09-25 17:20:19 +0300

    Update test.py

--- test.py
+++ test.py
@@ -167,3 +167,51 @@ if __name__ == '__main__':
     ex = Example()
     ex.show()
     sys.exit(app.exec())
+
+    
+    ==========================================
+    
+    
+import sys
+
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QLabel
+
+
+class FirstForm(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 300)
+        self.setWindowTitle('Главная форма')
+
+        self.btn = QPushButton('Другая форма', self)
+        self.btn.resize(100, 100)
+        self.btn.move(100, 100)
+
+        self.btn.clicked.connect(self.open_second_form)
+
+    def open_second_form(self):
+        self.second_form = SecondForm(self, "Данные для второй формы")
+        self.second_form.show()
+
+
+class SecondForm(QWidget):
+    def __init__(self, *args):
+        super().__init__()
+        self.initUI(args)
+
+    def initUI(self, args):
+        self.setGeometry(300, 300, 300, 300)
+        self.setWindowTitle('Вторая форма')
+        self.lbl = QLabel(args[-1], self)
+        self.lbl.adjustSize()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = FirstForm()
+    ex.show()
+    sys.exit(app.exec())
