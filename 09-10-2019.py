61f073d4a4dd78c166d1bc3e117fee1008feb498
Hyppocrat <djsh92@mail.ru>
2019-10-12 16:57:24 +0300

    Update 09-10-2019.py

--- 09-10-2019.py
+++ 09-10-2019.py
@@ -97,3 +97,39 @@ if __name__ == '__main__':
     ex = Example()
     ex.show()
     sys.exit(app.exec())
+
+    
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtGui import QPainter, QColor
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 600, 600)
+        self.setWindowTitle('Рисование')
+        self.show()
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        self.drawFlag(qp)
+        qp.end()
+
+    def drawFlag(self,qp):
+        qp.setBrush(QColor(255, 255, 255))
+        qp.drawRect(30, 30, 480, 120)
+        qp.setBrush(QColor(0, 0, 255))
+        qp.drawRect(30, 150, 480, 120)
+        qp.setBrush(QColor(255, 0, 0))
+        qp.drawRect(30, 270, 480, 120)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())    
